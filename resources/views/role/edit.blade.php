f2bc20c01d07584680a995fb38a0eb1988d04a46
itsayla <alyafatihah11@gmail.com>
2026-06-05 14:58:51 +0700

    additional di radio button edit.blade.php

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -16,8 +16,8 @@
             </div>
             <div class="mb-3">
                 <label for="">Status *</label>
-                <input type="radio" name="is_active" value="1" checked> Active
-                <input type="radio" name="is_active" value="0"> Inactive
+                <input type="radio" name="is_active" value="1" {{ $edit->is_active == 1 ? 'checked' : '' }}> Active
+                <input type="radio" name="is_active" value="0" {{ $edit->is_active == 0 ? 'checked' : '' }}> Inactive
             </div>
             <button class="btn btn-primary" type="submit">Save</button>
             <a href="{{ url()->previous() }}" class="btn btn-outline-secondary">Back</a>
